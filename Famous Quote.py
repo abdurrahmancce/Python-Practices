aa399fb11a1107eff9ef4152db3fee863648fec8
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-02-22 14:50:27 +0600

    Add files via upload

---
+++ Famous Quote.py
@@ -0,0 +1,17 @@
+Quote_Einstein= "Albert Einstein once said, 'A person who never made a mistake never tried anything new.'"
+print(Quote_Einstein)
+
+Quote_Edison = "Thomas Edison once said, 'I have not failed. I've just found 10,000 ways that won't work.'"
+print(Quote_Edison)
+
+Quote_Maya = "Maya Angelou once said, 'You will face many defeats in life, but never let yourself be defeated.'"
+print(Quote_Maya)
+
+Quote_Nelson = "Nelson Mandela once said, 'It always seems impossible until it’s done.'"
+print(Quote_Nelson)
+
+Quote_Steve = "Steve Jobs once said, 'Innovation distinguishes between a leader and a follower.'"
+print(Quote_Steve)
+
+Quote_Confucius = "Confucius once said, 'It does not matter how slowly you go as long as you do not stop.'"
+print(Quote_Confucius)
---
+++ Full_name.py
@@ -0,0 +1,8 @@
+first_name = "Abdur"
+last_name = "Rahman"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(f"Hello, {full_name.title()}! What a nice day to learn Python.")
+
+print("Languages:\nPython\nC\nJavaScript")
