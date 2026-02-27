6061aefb27d019fec318db5b115d1241085a6d77
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-02-27 22:26:08 +0600

    Add files via upload

---
+++ removing.py
@@ -0,0 +1,15 @@
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+print(motorcycles)
+
+del motorcycles[0] 
+print(motorcycles)
+
+del motorcycles[1] 
+print(motorcycles)
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop() 
+print(motorcycles)
+print(popped_motorcycle)
