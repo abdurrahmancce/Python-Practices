9bc90eb7822764bcff9a1fcd2d72d29a70ec87d1
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-01 22:24:06 +0600

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,8 @@
+players = ['Hamza', 'Mohamed Salah', 'Paul Pogba', 'Mesut Özil', 'Karim Benzema', 'Sadio Mane', 'Riyad Mahrez'] 
+print(players[0:3]) # Hamza, Mohamed Salah, Paul Pogba
+
+print(players[1:4]) # Mohamed Salah, Paul Pogba, Mesut Özil
+
+print(players[:4]) # Hamza, Mohamed Salah, Paul Pogba, Mesut Özil
+
+print(players[3:]) # Mesut Özil, Karim Benzema, Sadio Mane, Riyad Mahrez
