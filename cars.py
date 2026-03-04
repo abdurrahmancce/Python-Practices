44490ec8b9e471265481f141b797eecf60e3a69b
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-04 23:21:37 +0600

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,16 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars) #['audi', 'bmw', 'subaru', 'toyota']
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars) #['toyota', 'subaru', 'bmw', 'audi']
+
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print("Here is the original list:")
+# print(cars)
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+# print("\nHere is the original list again:")
+# print(cars)
