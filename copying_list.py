6fc27e7a0ec5960a9d32f4ed331e82c3abc8c097
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-04 23:20:57 +0600

    Add files via upload

---
+++ copying_list.py
@@ -0,0 +1,6 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:") # pizza, falafel, carrot cake
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods) # pizza, falafel, carrot cake
