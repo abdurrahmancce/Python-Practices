4b7a58a40aeddf8eaafa0be8aa1a4a5ddcfd35d3
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-09 00:00:18 +0600

    Add files via upload

---
+++ delete_dictionary_value.py
@@ -0,0 +1,6 @@
+person={'name': 'Sky', 'age': 24, 'city': 'Dhaka', 'occupation': 'Engineer'}
+print(person)
+
+del person['age'] # Deleting the 'age' key-value pair from the dictionary
+del person['city'] # Deleting the 'city' key-value pair from the dictionary
+print(person)
