33f34831c9a57103142aa510c9ea06a7be1bf26c
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-08 23:46:06 +0600

    Add files via upload

---
+++ empty_dictionary.py
@@ -0,0 +1,11 @@
+# occupation = {}
+# occupation['name'] = 'Data Scientist'
+# occupation[type('name')] = 'Data Scientist'
+# occupation['name'] = 'Software Engineer'
+# print(occupation)
+
+fruits = {}
+fruits['name'] = 'Apple', 'Orange', 'Banana'
+fruits['color'] = 'Red', 'Orange', 'Yellow'
+fruits['size'] = 'Medium', 'Semi-Medium', 'Small'
+print(fruits)
