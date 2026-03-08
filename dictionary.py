4aba9df6bc711212f3615cdfd2fca3e67e43b2f6
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-08 23:46:47 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+person = {
+    'name': 'Abdur Rahman',
+    'age': 24,
+    'city': 'Bangladesh',
+    'occupation': 'Engineer'
+}
+
+print(person)
+print(f"\nName: {person['name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
+print(f"Occupation: {person['occupation']}")
