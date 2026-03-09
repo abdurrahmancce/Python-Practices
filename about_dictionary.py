7a1bda9c4ccc8d2f66cdbc99e2121c56640028fe
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-09 14:57:17 +0600

    Add files via upload

---
+++ about_dictionary.py
@@ -0,0 +1,9 @@
+details={'first_name': 'Abdur', 
+        'last_name': 'Rahman', 
+        'age': 24, 
+        'city': 'Dhaka',
+        'occupation': 'Engineer',
+        'hobbies': ['Programming', 'Traveling', 'Cooking'],
+        'skills': {'Python': 'Java', 'JavaScript': 'C', 'SQL': 'Django'},
+        'nationality': 'Bangladeshi'}
+print(details)
