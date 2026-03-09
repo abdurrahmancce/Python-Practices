1e518ca5082dc9e4c3fc26b5ab46d917957bdd67
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-09 20:46:51 +0600

    Add files via upload

---
+++ favourite_num.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'akash': 11,
+    'batash': 22,
+    'pinky': 33,
+    'diya': 44,
+    'ethan': 55,
+}
+
+for name, number in favorite_numbers.items():
+    print(f"{name.title()}'s favorite number is {number}.")
