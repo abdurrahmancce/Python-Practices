70e4f374b6fb46ac33a35c7b54dee3d840e757e9
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-03-11 11:52:07 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}! How are you?")
+
+age = input("How old are you? ")
+print(f"You are {age} years old.")
