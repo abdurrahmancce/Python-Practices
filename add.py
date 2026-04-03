bd64853464b5728f3f582e30a5ef9dd3730a99d3
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-04-03 19:38:19 +0600

    Add files via upload

---
+++ add.py
@@ -0,0 +1,8 @@
+# Initialize a set with 5 elements
+my_set = {1, 2, 3, 4, 5}
+
+# Add a new element (6) to the set
+my_set.add(6)  # This will add 6 to the set
+
+# Print the updated set
+print(my_set)
