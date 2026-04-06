bceb2db8b9446a01a905dc53d209b4182d0e7129
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-04-07 00:22:58 +0600

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,4 @@
+fruits = {"apple", "banana", "guava", "mango"}
+item = fruits.pop()
+print(fruits) # Output: {'banana', 'guava'}
+print(item) # Output: 'apple' (or 'banana' or 'guava', as sets are unordered)
