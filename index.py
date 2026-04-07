62e9b58a24e7d898d74a7afdff73dc7bff2a47d0
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2026-04-08 00:12:12 +0600

    Add files via upload

---
+++ index.py
@@ -0,0 +1,3 @@
+number = (10, 20, 30, 40, 50)
+print("Index of {} is: {}".format(20, number.index(20))) # Output: Index of 20 is: 1
+print("Index of {} is: {}".format(40, number.index(40))) # Output: Index of 40 is: 3
